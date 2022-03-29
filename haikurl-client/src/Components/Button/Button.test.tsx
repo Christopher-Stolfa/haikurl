0a5f3b86469eecd06a34eba0cdfd686e4343574f
Chribtoper <xcstolfax@gmail.com>
2022-03-29 12:12:44 -0700

    Added a test that checks if element children are rendered

--- haikurl-client/src/Components/Button/Button.test.tsx
+++ haikurl-client/src/Components/Button/Button.test.tsx
@@ -3,6 +3,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Button from './Button';
 
-test('Renders a submit button', () => {
+test('Should show a submit button', () => {
   render(<Button />);
+});
+
+test('Button should accept child elements and render text', () => {
+  render(<Button type="submit">submit</Button>);
+  const buttonElement = screen.getByTestId('Button');
+  expect(buttonElement).toHaveTextContent('submit');
 });
