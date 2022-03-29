302f29fe473ec59de976d9cfc770ce8ee40705c5
Chribtoper <xcstolfax@gmail.com>
2022-03-29 12:35:08 -0700

    Tests the onClick handler to check if it has been fired

--- haikurl-client/src/Components/Button/Button.test.tsx
+++ haikurl-client/src/Components/Button/Button.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Button from './Button';
 
-test('Should show a submit button', () => {
+test('Should show a button', () => {
   render(<Button />);
 });
 
@@ -11,4 +11,11 @@ test('Button should accept child elements and render text', () => {
   render(<Button type="submit">submit</Button>);
   const buttonElement = screen.getByTestId('Button');
   expect(buttonElement).toHaveTextContent('submit');
+});
+
+test('Calls onClick prop when clicked', () => {
+  const handleClick = jest.fn();
+  render(<Button onClick={handleClick}>submit</Button>);
+  fireEvent.click(screen.getByText(/submit/i));
+  expect(handleClick).toHaveBeenCalledTimes(1);
 });
