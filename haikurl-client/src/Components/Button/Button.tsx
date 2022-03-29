fab5a63cf03b9b1101c89780850fe2d126561f14
Chribtoper <xcstolfax@gmail.com>
2022-03-29 11:19:22 -0700

    Added a new prop called 'type' which can only be a string named 'submit' or 'button'

--- haikurl-client/src/Components/Button/Button.tsx
+++ haikurl-client/src/Components/Button/Button.tsx
@@ -4,18 +4,20 @@ import React from 'react';
  * @typedef Props
  * @prop {(event: React.MouseEvent<HTMLButtonElement>)=>void} onClick - Callback that handles the button click event
  * @prop {React.ReactNode} children - Child elements
+ * @prop {submit | button} type - The type of button, must be 'submit' or 'button'
  */
 type Props = {
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
   children?: React.ReactNode;
+  type?: 'submit' | 'button';
 };
 
 /**
  * Custom button component
  * @param Props
  */
-const Button = ({ onClick, children }: Props) => (
-  <button type="submit" className="Button" onClick={onClick}>
+const Button = ({ onClick, children, type = 'button' }: Props) => (
+  <button type={type} className="Button" onClick={onClick}>
     {children}
   </button>
 );
