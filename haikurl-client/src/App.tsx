3dea413d270393a691de477542379f5e34b4c98e
Chribtoper <xcstolfax@gmail.com>
2022-03-29 18:02:18 -0700

    Now renders TextField and button in an output group. This group will contain the new haiku url and the copy button will copy it

--- haikurl-client/src/App.tsx
+++ haikurl-client/src/App.tsx
@@ -1,10 +1,11 @@
 /*global chrome*/
 import React, { useEffect, useState } from 'react';
+import './App.css';
 import Input from './Components/Input/Input';
 import Header from './Components/Header/Header';
 import Form from './Components/Form/Form';
 import Button from './Components/Button/Button';
-import './App.css';
+import TextField from './Components/TextField/TextField';
 
 const App = () => {
   const [url, setUrl] = useState<string>('');
@@ -28,6 +29,12 @@ const App = () => {
         <div className="App-Input-Group">
           <Input value={url} onChange={handleOnChange} />
           <Button type="submit">Submit</Button>
+        </div>
+        <div className="App-Output-Group">
+          <TextField />
+          <div className="App-Bottom-Right">
+            <Button type="button">Copy</Button>
+          </div>
         </div>
       </Form>
     </div>
